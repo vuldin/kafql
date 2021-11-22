80a9fb3315b052cc0f5c0d1c5bb92b4f04cc8f03
Josh Purcell <josh.purcell@ibm.com>
2021-11-22 14:45:49 -0500

    add TODOs to client/server graphql projects, fire user events userCreated and userMoved and getUsers periodically, handle them on client side

--- graphql/src/schema.ts
+++ graphql/src/schema.ts
@@ -1,34 +1,83 @@
-import { GraphQLID, GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+import {
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql'
 import { setTimeout } from 'timers/promises'
 
 const userMovedType = new GraphQLObjectType({
   name: 'userMovedType',
   fields: {
     userId: { type: GraphQLID },
+    latitude: { type: GraphQLInt },
     longitude: { type: GraphQLInt },
   },
 })
 
-async function* userMovedSubscription() {
-  const vals = [
-    { userId: '1a2b3c', longitude: 0 },
-    { userId: '1a2b3c', longitude: 1 },
-    { userId: '1a2b3c', longitude: 2 },
-    { userId: '1a2b3c', longitude: 3 },
-    { userId: '1a2b3c', longitude: 4 },
-    { userId: '1a2b3c', longitude: 3 },
-    { userId: '1a2b3c', longitude: 2 },
-    { userId: '1a2b3c', longitude: 1 },
-  ]
-  /*
-  for (const val of vals) {
-    yield { userMoved: val }
+const userType = new GraphQLObjectType({
+  name: 'userType',
+  fields: {
+    userId: { type: GraphQLID },
+    latitude: { type: GraphQLInt },
+    longitude: { type: GraphQLInt },
+    patience: { type: GraphQLInt },
+  },
+})
+
+// TODO move interface and users array out of schema file
+interface User {
+  userId: string
+  latitude: number
+  longitude: number
+  patience?: number
+}
+
+let users: User[] = [
+  {
+    userId: '1a2b3c',
+    latitude: 1,
+    longitude: 0,
+    patience: 5,
+  },
+]
+
+// TODO subscribe to userCreated topic
+async function* userCreatedEvent() {
+  yield {
+    userCreated: users[0],
   }
-  */
+}
+
+// TODO subscribe to userMoved topic
+async function* userMovedEvent() {
+  const vals = [
+    { latitude: 1, longitude: 1 },
+    { latitude: 1, longitude: 2 },
+    { latitude: 1, longitude: 3 },
+    { latitude: 1, longitude: 4 },
+    { latitude: 1, longitude: 3 },
+    { latitude: 1, longitude: 2 },
+    { latitude: 1, longitude: 1 },
+    { latitude: 1, longitude: 0 },
+  ]
 
   let num = 0
   while (true) {
-    yield { userMoved: vals[num % vals.length] }
+    // TODO stop modifying users here (do it in mutation, etc.)
+    users[0] = {
+      ...users[0],
+      ...vals[num % vals.length],
+    }
+    yield {
+      userMoved: {
+        userId: users[0].userId,
+        ...vals[num % vals.length],
+      },
+    }
     num += 1
     await setTimeout(2000)
   }
@@ -42,14 +91,29 @@ export const schema = new GraphQLSchema({
         type: GraphQLString,
         resolve: () => 'world',
       },
+      users: {
+        type: new GraphQLNonNull(new GraphQLList(userType)),
+        resolve: () => users,
+      },
+      user: {
+        type: userType,
+        args: {
+          userId: { type: GraphQLID },
+        },
+        resolve: (_, { userId }) => users.filter((user) => user.userId === userId),
+      },
     },
   }),
   subscription: new GraphQLObjectType({
     name: 'Subscription',
     fields: {
+      userCreated: {
+        type: userType,
+        subscribe: userCreatedEvent,
+      },
       userMoved: {
         type: userMovedType,
-        subscribe: userMovedSubscription,
+        subscribe: userMovedEvent,
       },
     },
   }),
--- simple-graphql-client/src/index.ts
+++ simple-graphql-client/src/index.ts
@@ -16,16 +16,52 @@ const client = createClient({
   retryAttempts: Infinity,
 })
 const execute = getExecuteFunction(client)
-const query = 'subscription { userMoved { userId longitude }}'
-//const query = 'query { hello }'
 
-;(async () => {
+async function userCreatedEvents() {
+  const userCreatedQuery = 'subscription { userCreated { userId latitude longitude patience }}'
   try {
-    const subscription = execute({ query })
+    const subscription = execute({ query: userCreatedQuery })
     for await (const result of subscription) {
       console.log(result)
     }
   } catch (err) {
     console.error(err)
   }
-})()
+}
+
+async function userMovedEvents() {
+  const userMovedQuery = 'subscription { userMoved { userId latitude longitude }}'
+  try {
+    const subscription = execute({ query: userMovedQuery })
+    for await (const result of subscription) {
+      console.log(result)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function getUsers() {
+  const usersQuery = 'query { users { userId latitude longitude patience }}'
+  try {
+    const subscription = execute({ query: usersQuery })
+    for await (const result of subscription) {
+      console.log(JSON.stringify(result, null, 1))
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// TODO these functions should only start listening for events
+// server should not be creating user or moving user based on these calls
+// move user creation and movement to mutations, keep subscriptions for handling events
+userCreatedEvents()
+userMovedEvents() // kicks off user movement
+async function regularlyGetUsers() {
+  while (true) {
+    await getUsers()
+    await setTimeout(4000)
+  }
+}
+regularlyGetUsers()
